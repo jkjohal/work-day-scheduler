8b99b203d79b3c9ab4c067bb3e0061540cacbcf7
Jeevan Johal <jkjohal@ucdavis.edu>
2023-02-05 12:11:23 -0800

    local storage set item

--- Assets/script.js
+++ Assets/script.js
@@ -8,10 +8,19 @@ $('.saveBtn').on('click', function(){
     var value=$(this).siblings('textarea').val()
 
     localStorage.setItem(key, value)
-})
+});
 
 
-//use local storage.getitem to retrive content from local sotrage and make sure that it stays on the page when we reload. 
+//use local storage.getitem to retrive content from local storage and make sure that it stays on the page when we reload. 
 
+// function loadEntries = localStorage.getItem("textarea")
+
+$("textarea").on("load", function(){
+    
+    var key=$(this).parent().attr('id')
+
+    var lastEntry = localStorage.getItem(key)
+    (this).textContent = lastEntry
+});
 
 //create a function that will check the current time vs the timeblock time ...hint use the ID and add the appropriate class of past present or future 
